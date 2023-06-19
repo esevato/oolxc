aa31f1c7339ee75daac301bbc6d2539b8bc8b1bd
EseVato <60022925+esevato@users.noreply.github.com>
2023-06-18 22:14:09 -0400

    Update and rename oolxc-install.sh to oo-install.sh
    should work for any debian based machine
    run as root

--- oo-install.sh
+++ oo-install.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 while true; do
     read -p "This will Install OpenObserve, Proceed(y/n)?" yn
     case $yn in
@@ -8,36 +7,35 @@ while true; do
     *) echo "Please answer yes or no." ;;
     esac
 done
-echo -e updating container
+echo -e updating debian
 apt-get update
 echo -e installing dependancies
 apt-get install curl -y
-
-mkdir /etc/oolxc
-mkdir /usr/share/oolxc
+mkdir /etc/oo
+mkdir /usr/share/oo
 mkdir /data
-mkdir /data/oolxc
+mkdir /data/oo
 wget https://github.com/openobserve/openobserve/releases/download/v0.4.7/openobserve-v0.4.7-linux-amd64.tar.gz
-tar -zxvf openobserve-v0.4.7-linux-amd64.tar.gz -C /usr/share/oolxc
+tar -zxvf openobserve-v0.4.7-linux-amd64.tar.gz -C /usr/share/oo
 rm openobserve-v0.4.7-linux-amd64.tar.gz
 echo -e set environment
-cat << EOF > /etc/oolxc.env
-ZO_ROOT_USER_EMAIL = "oolxc@example.com"
-ZO_ROOT_USER_PASSWORD = "OpenObserveLXC"
-ZO_DATA_DIR = "/data/oolxc"
+cat << EOF > /etc/oo.env
+ZO_ROOT_USER_EMAIL = "oo@example.com"
+ZO_ROOT_USER_PASSWORD = "OpenObserve"
+ZO_DATA_DIR = "/data/oo"
 EOF
-echo -e set up oolxc service
-cat << EOF > /usr/lib/systemd/system/oolxc.service
+echo -e set up oo service
+cat << EOF > /usr/lib/systemd/system/oo.service
 [Unit]
-Description=The OpenObserve container
+Description=The OpenObserve Server
 After=syslog.target network-online.target remote-fs.target nss-lookup.target
 Wants=network-online.target
 
 [Service]
 Type=simple
 LimitNOFILE=65535
-EnvironmentFile=/etc/oolxc.env
-ExecStart=/usr/share/oolxc/openobserve
+EnvironmentFile=/etc/oo.env
+ExecStart=/usr/share/oo/openobserve
 ExecStop=/bin/kill -s QUIT $MAINPID
 Restart=on-failure
 
@@ -47,11 +45,10 @@ EOF
 echo -e reload daemon
 systemctl daemon-reload
 echo -e enable service to start at boot
-systemctl enable oolxc
-echo -e start oolxc service
-systemctl start oolxc
-msg_ok "service started"
-systemctl status oolxc
+systemctl enable oo
+echo -e start oo service
+systemctl start oo
+systemctl status oo
 echo -e you can now log in at http://<yourIP>:5080
-echo -e login = oolxc@example.com
-echo -e password = OpenObserveLXC
+echo -e login = oo@example.com
+echo -e password = OpenObserve
